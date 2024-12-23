fee6980961ff7289c0f029abf06a9c44e6368301
Marco Milanta <marco.milanta@gmail.com>
2024-12-23 19:39:22 +0100

    fix: bug

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from api import api
 from ui import ui
 
 app = FastAPI()
-app.mount("/api/v1", api)
-app.mount("/", ui)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(GZipMiddleware)
+app.mount("/api/v1", api)
+app.mount("/", ui)

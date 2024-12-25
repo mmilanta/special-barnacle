b4c54d07b929a46857a394ee9652b7a9f48163f1
Marco Milanta <marco.milanta@gmail.com>
2024-12-25 22:45:19 +0100

    fix: disable https

--- src/app.py
+++ src/app.py
@@ -10,6 +10,17 @@ from oauth import oauth_app
 app = FastAPI()
 app.add_middleware(GZipMiddleware)
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
+
+
+# get rid of HSTS header (https)
+@app.middleware("http")
+async def remove_hsts_header(request, call_next):
+    response = await call_next(request)
+    # Add or reset the Strict-Transport-Security header
+    response.headers["Strict-Transport-Security"] = "max-age=0"
+    return response
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/api/v1", api)
 app.mount("/auth", oauth_app)
